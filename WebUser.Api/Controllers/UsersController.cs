828368442ceabc20f192f177b7fda6556fc5d2c0
sandip0107-projecttree <sandip.dhorajiya.projecttree@outlook.com>
2025-01-21 11:09:46 +0530

    Test Deepsource

--- WebUser.Api/Controllers/UsersController.cs
+++ WebUser.Api/Controllers/UsersController.cs
@@ -24,6 +24,7 @@ namespace WebUser.Api.Controllers
         {
             id = 0;
             var user = _userService.GetUserById(id);
+            var user1 = _userService.GetUserById(id);
             if (user == null)
             {
                 return NotFound();
