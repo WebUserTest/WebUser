44c42905e471f50196c8f520d3f7f065a53745e5
sandip0107-projecttree <sandip.dhorajiya.projecttree@outlook.com>
2025-01-24 09:17:01 +0530

    Removed unused code

--- WebUser.Api/Controllers/UsersController.cs
+++ WebUser.Api/Controllers/UsersController.cs
@@ -2,63 +2,61 @@
 using WebUser.Api.Models;
 using WebUser.Api.Services;
 
-namespace WebUser.Api.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
-    {
-        private readonly IUserService _userService;
-        public UsersController(IUserService userService)
-        {
-            _userService = userService ?? throw new ArgumentNullException(nameof(userService));
-        }
-        [HttpGet]
-        public IActionResult GetAllUsers()
-        {
-            var users = _userService.GetAllUsers();
-            return Ok(users);
-        }
-        [HttpGet("{id}")]
-        public IActionResult GetUserById(int id)
-        {
-            id = 0;
-            var user = _userService.GetUserById(id);
-            var user1 = _userService.GetUserById(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            return Ok(user);
-        }
-        [HttpPost]
-        public IActionResult AddUser([FromBody] User user)
-        {
-            _userService.AddUser(user);
-            return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, user);
-        }
-        [HttpPut("{id}")]
-        public IActionResult UpdateUser(int id, [FromBody] User user)
-        {
-            if (id != user.Id)
-            {
-                return BadRequest();
-            }
-            _userService.UpdateUser(user);
-            return NoContent();
-        }
-        [HttpDelete("{id}")]
-        public IActionResult DeleteUser(int id)
-        {
-            _userService.DeleteUser(id);
-            return NoContent();
-        }
+namespace WebUser.Api.Controllers;
 
-        [HttpGet("{Name}")]
-        public IActionResult GetUserById(string Name)
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _userService;
+    public UsersController(IUserService userService)
+    {
+        _userService = userService ?? throw new ArgumentNullException(nameof(userService));
+    }
+    [HttpGet]
+    public IActionResult GetAllUsers()
+    {
+        var users = _userService.GetAllUsers();
+        return Ok(users);
+    }
+    [HttpGet("{id}")]
+    public IActionResult GetUserById(int id)
+    {
+        id = 0;
+        var user = _userService.GetUserById(id);
+        if (user == null)
         {
-            Name = "Sandip";
-            return Ok(Name);
+            return NotFound();
         }
+        return Ok(user);
+    }
+    [HttpPost]
+    public IActionResult AddUser([FromBody] User user)
+    {
+        _userService.AddUser(user);
+        return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, user);
+    }
+    [HttpPut("{id}")]
+    public IActionResult UpdateUser(int id, [FromBody] User user)
+    {
+        if (id != user.Id)
+        {
+            return BadRequest();
+        }
+        _userService.UpdateUser(user);
+        return NoContent();
+    }
+    [HttpDelete("{id}")]
+    public IActionResult DeleteUser(int id)
+    {
+        _userService.DeleteUser(id);
+        return NoContent();
+    }
+
+    [HttpGet("{Name}")]
+    public IActionResult GetUserById(string Name)
+    {
+        Name = "Sandip";
+        return Ok(Name);
     }
 }
